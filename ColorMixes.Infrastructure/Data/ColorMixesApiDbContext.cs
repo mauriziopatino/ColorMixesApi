01b9e3a0fff1ca2c8971a246434b5459bd3c8e43
Maurizio Patiño <mauriziopatino@hotmail.com>
2023-10-15 15:37:28 -0600

    Entity framework configuration and db context

---
+++ ColorMixes.Infrastructure/Data/ColorMixesApiDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorMixes.Infrastructure.Data
+{
+    public class ColorMixesApiDbContext : DbContext
+    {
+        public ColorMixesApiDbContext(DbContextOptions<ColorMixesApiDbContext> options) : base(options)
+        {
+        }
+
+        protected ColorMixesApiDbContext()
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .UseCollation("Modern_Spanish_CI_AS");
+        }
+    }
+}
