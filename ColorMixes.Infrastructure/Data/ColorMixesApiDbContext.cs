b854482457bce38459417b35420f9f3562d9dae9
Maurizio Patiño <mauriziopatino@hotmail.com>
2023-10-15 16:32:38 -0600

    Color model and migration

--- ColorMixes.Infrastructure/Data/ColorMixesApiDbContext.cs
+++ ColorMixes.Infrastructure/Data/ColorMixesApiDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ColorMixes.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,8 @@ namespace ColorMixes.Infrastructure.Data
         protected ColorMixesApiDbContext()
         {
         }
+
+        public DbSet<Color> Colors { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
---
+++ ColorMixes.Infrastructure/Models/Color.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorMixes.Infrastructure.Models
+{
+    public class Color
+    {
+        [Column(Order = 1)]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
